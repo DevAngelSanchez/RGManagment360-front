09b13a88e0f6073bd5bfd82a8ce4c3328a617ec2
DevAngelSanchez <dev.angelrsanchez@gmail.com>
2024-07-16 20:55:41 -0400

    Implementado nextauth para inicio de sesion y mantener la sesion

---
+++ auth.ts
@@ -0,0 +1,59 @@
+import NextAuth from "next-auth"
+import Credentials from "next-auth/providers/credentials"
+// Your own logic for dealing with plaintext password strings; be careful!
+
+const apiUrl = process.env.API_URL || "http://localhost:3001/";
+
+export const { handlers, signIn, signOut, auth } = NextAuth({
+  providers: [
+    Credentials({
+      // You can specify which fields should be submitted, by adding keys to the `credentials` object.
+      // e.g. domain, username, password, 2FA token, etc.
+      credentials: {
+        email: {
+          label: 'Email',
+          type: 'email',
+          placeholder: 'johndoe@gmail.com'
+        },
+        password: {
+          label: 'Contraseña',
+          type: 'password',
+          placeholder: '********'
+        },
+      },
+      authorize: async (credentials) => {
+
+        if (!credentials) {
+          return null;
+        }
+
+        const res = await fetch(`${process.env.API_URL}auth/login`, {
+          method: 'POST',
+          headers: { 'Content-Type': 'application/json' },
+          body: JSON.stringify({
+            email: credentials.email,
+            password: credentials.password
+          })
+        });
+
+        console.log(res);
+
+        if (!res.ok) {
+          console.error('Hubo un error en la consulta');
+          return null;
+        }
+
+        const user = await res.json();
+
+        console.log(user)
+
+        // Si la API devuelve un objeto de usuario válido, devuélvelo
+        if (user) {
+          return user;
+        } else {
+          return null;
+        }
+      },
+    }),
+  ],
+})
