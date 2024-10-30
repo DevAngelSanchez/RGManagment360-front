76a84195465c13f2732cc7029e0e8c020aef8a82
DevAngelSanchez <dev.angelrsanchez@gmail.com>
2024-10-30 13:27:28 -0400

    avances en tabla de services

--- lib/fetch.ts
+++ lib/fetch.ts
@@ -1,5 +1,6 @@
 import { apiUrl } from "@/auth.config";
 import { Category, Property, Subcategory, Supplier, Task, User } from "./types";
+import { SubcategoryType } from "@/app/(protected)/service-provider/tasks/data/schema";
 
 interface FetchResult<Type> {
   data?: Type;
@@ -36,7 +37,7 @@ const fetchData = async <Type>(url: string): Promise<FetchResult<Type>> => {
 export const fetchUsers = () => fetchData<User[]>(`${apiUrl}api/users`);
 export const fetchCategories = () => fetchData<Category[]>(`${apiUrl}api/categories`);
 export const fetchSuppliers = () => fetchData<Supplier[]>(`${apiUrl}api/suppliers`);
-export const fetchSubcategories = () => fetchData<Subcategory[]>(`${apiUrl}api/subcategories`);
+export const fetchSubcategories = () => fetchData<SubcategoryType[]>(`${apiUrl}api/subcategories`);
 export const fetchProperties = () => fetchData<Property[]>(`${apiUrl}api/properties`);
 export const fetchServiceProviders = () => fetchData<User[]>(`${apiUrl}api/users/by-role/SERVICE_PROVIDER`);
 export const fetchCustomers = () => fetchData<User[]>(`${apiUrl}api/users/by-role/CUSTOMER`);
