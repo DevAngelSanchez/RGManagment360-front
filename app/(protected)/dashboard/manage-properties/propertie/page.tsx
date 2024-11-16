3132ebe953bb93b1403263c801fbc3ee4ceee15d
Jose Martinez <josemar77jam@gmail.com>
2024-11-16 19:43:41 -0400

    singular view component

--- app/(protected)/dashboard/manage-properties/propertie/page.tsx
+++ app/(protected)/dashboard/manage-properties/propertie/page.tsx
@@ -24,12 +24,14 @@ export default async function TaskPage() {
   }));
 
   //MOCK DATA
-  const task = {
-    title: "Task 1",
-    description: "Inspect the structure of the home for cracks or damage to the foundation and walls. Check pipes for leaks or drips. Clean appliances, such as the washing machine and dryer drum, and the refrigerator.",
-    dueDate: "2024-11-20",
-    priority: "High",
-    status: "Pending",
+  const propertie = {
+    propertyName: "My Property",
+    address: "123 Main St, Anytown, USA",
+    customerName: "Jane Doe",
+    phoneNumber: "555-555-5555",
+    email: "T0k6o@example.com", 
+    description: "This is a description of the property. It may include details such as size, location, and any other relevant information.",
+
   };
 
   return (
@@ -37,7 +39,7 @@ export default async function TaskPage() {
       <main className="w-full h-full bg-slate-50 ">
         <section className="p-4 overflow-y-auto">
           <div>
-            <SingularView mode="task" task={task} /> {/* task={tasksData?.[0]} */}
+            <SingularView mode="propertie" propertie={propertie} /> {/* task={tasksData?.[0]} */}
           </div>
         </section>
       </main>
